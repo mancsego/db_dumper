5354182eda07273e0b14ae8eece37e236ef9ad07
Istvan Abraham <mancsego@gmail.com>
2024-12-02 16:20:59 +0100

    Export table definition based on config

--- src/exporter/worker.ts
+++ src/exporter/worker.ts
@@ -1,21 +1,51 @@
 import { parentPort } from 'worker_threads'
+import { getConnection } from '#src/exporter/db'
+import { loadConfiguration } from '#src/exporter/config/configLoader'
 
 const fileTemplate = 'dump.sql'
 
 parentPort?.on('message', async (uuid) => {
   try {
-    console.log('start')
-
-    setTimeout(() => {
-      parentPort?.postMessage({
-        success: true,
-        message: `Task ${uuid} successfully completed. Dump file: ${fileTemplate}`
-      })
-    }, 5000)
-  } catch (error) {
-    parentPort?.postMessage({
-      success: false,
-      message: `Task ${uuid} failed because ${(error as Error).message}`
-    })
+    await buildDump(uuid)
+  } catch (e) {
+    errorHandler(uuid, e as Error)
   }
 })
+
+const buildDump = async (uuid: string) => {
+  const tables = await loadConfiguration()
+
+  createTableStatements(Object.keys(tables))
+
+  parentPort?.postMessage({
+    success: true,
+    message: `Task ${uuid} successfully completed. Dump file: ${fileTemplate}`
+  })
+}
+
+const createTableStatements = async (tables: Array<string>) => {
+  const { connection } = await getConnection()
+
+  const results = await Promise.all(
+    tables.map(async (table) => {
+      const [rows] = await connection.query(`SHOW CREATE TABLE \`${table}\``)
+      return rows // Adjust based on actual result structure
+    })
+  )
+
+  return results.reduce((collected, next) => {
+    return `${collected}${(next as Array<Record<string, string>>)[0]['Create Table']};\n\n`
+  }, '')
+}
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const createDataStatements = async () => {
+  return ''
+}
+
+const errorHandler = (uuid: string, { message }: Error) => {
+  parentPort?.postMessage({
+    success: false,
+    message: `Task ${uuid} failed because ${message}`
+  })
+}
