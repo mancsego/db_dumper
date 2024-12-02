b420ef49754ee6a36900ffc430d4b21b6213ad3a
Istvan Abraham <mancsego@gmail.com>
2024-12-02 15:45:31 +0100

    Fix config loading with typescript

--- src/exporter/config/configLoader.ts
+++ src/exporter/config/configLoader.ts
@@ -1,13 +1,13 @@
 const loadConfiguration = async () => {
   try {
-    const { default: config } = await import('#root/definition.local.js')
+    const { default: config } = await import('#root/definition.local')
     return config
   } catch (_) {
     console.warn(
-      'Definition module (#root/definition.js) missing, loading empty configuration.'
+      'Definition module (#root/definition.local.ts) missing, loading empty configuration.'
     )
 
-    const { default: config } = await import('#root/definition.dist.js')
+    const { default: config } = await import('#root/definition.dist')
     return config
   }
 }
