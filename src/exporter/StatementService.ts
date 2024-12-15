3d42de085b356eb38388f674f53ad7a5913f268f
Istvan Abraham <mancsego@gmail.com>
2024-12-15 17:08:58 +0100

    Restructure exporter and save up primaries for later

---
+++ src/exporter/StatementService.ts
@@ -0,0 +1,93 @@
+import { RowDataPacket } from 'mysql2'
+import {
+  ImportDefinition,
+  ExportTypes,
+  ConfigObject
+} from '#src/exporter/config/types'
+import { getConnection } from '#src/exporter/db'
+import { openPrimaryStream } from '#src/exporter/FileService'
+
+const stringify = ['object', 'string']
+const shouldStringify = (x: unknown) => stringify.includes(x as string)
+
+const createTableStatements = async (definition: ImportDefinition) => {
+  const { connection } = await getConnection()
+
+  const results = await Promise.all(
+    definition.map(async ({ table }) => {
+      const [rows] = await connection.query<RowDataPacket[]>(
+        `SHOW CREATE TABLE ${table}`
+      )
+      return rows
+    })
+  )
+
+  return results.reduce(
+    (collected: string, next: RowDataPacket[]) =>
+      `${collected}${next[0]['Create Table']};`,
+    ''
+  )
+}
+
+const createDataStatements = async (definition: ImportDefinition) => {
+  const { connection } = await getConnection()
+
+  let statements = ''
+  for (const config of definition) {
+    if (config.type === ExportTypes.STRUCTURE_ONLY) continue
+
+    const [rows] = await connection.query<RowDataPacket[]>(
+      `SELECT * FROM ${config.table}`
+    )
+
+    statements += _createInsertStatements(config, rows)
+  }
+
+  return statements
+}
+
+const _createInsertStatements = (
+  config: ConfigObject,
+  rows: RowDataPacket[]
+) => {
+  const stream = openPrimaryStream(config.table)
+
+  const values = rows
+    .reduce(
+      _valueReducer(config, (primary: string) => stream.save(primary)),
+      ''
+    )
+    .slice(0, -1)
+
+  const statement = `INSERT INTO ${config.table}VALUES${values};\n\n`
+  stream.close()
+
+  return statement
+}
+
+const _valueReducer =
+  (config: ConfigObject, clb: CallableFunction) =>
+  (values: string, next: Record<string, unknown>) => {
+    clb(next[config.primary ?? ''])
+
+    const row = Object.keys(next)
+      .reduce((collected: string, column: string) => {
+        return `${collected}${_getValue(next[column], _getFilterMethod(config.columns, column))}, `
+      }, '')
+      .slice(0, -2)
+
+    return `${values} (${row}),`
+  }
+
+const _getValue = (v: unknown, clb: CallableFunction) => {
+  const res = clb(v)
+
+  return shouldStringify(typeof v) ? `"${res}"` : res
+}
+
+const _getFilterMethod = (
+  columnConfig: Record<string, CallableFunction> | undefined,
+  column: string
+) => (columnConfig ?? {})[column] ?? ((v: unknown) => v)
+
+export { createTableStatements, createDataStatements }
--- src/exporter/worker.ts
+++ src/exporter/worker.ts
@@ -1,15 +1,12 @@
 import { parentPort } from 'worker_threads'
-import { RowDataPacket } from 'mysql2'
-import { getConnection } from '#src/exporter/db'
 import { loadConfiguration } from '#src/exporter/config/configLoader'
 import {
-  ConfigObject,
-  ExportTypes,
-  ImportDefinition
-} from '#src/exporter/config/types'
+  createTableStatements,
+  createDataStatements
+} from '#src/exporter/StatementService'
+import { cleanUpTmpFiles } from '#src/exporter/FileService'
 
 const fileTemplate = 'dump.sql'
-let definition: ImportDefinition = []
 
 parentPort?.on('message', async (uuid) => {
   try {
@@ -20,88 +17,19 @@ parentPort?.on('message', async (uuid) => {
 })
 
 const buildDump = async (uuid: string) => {
-  definition = await loadConfiguration()
+  const definition = await loadConfiguration()
 
-  const createTables = await createTableStatements()
-  const createData = await createDataStatements()
+  const createTables = await createTableStatements(definition)
+  const createData = await createDataStatements(definition)
 
-  console.log(createTables)
-  console.log(createData)
+  // console.log(createTables)
+  // console.log(createData)
 
   parentPort?.postMessage({
     success: true,
     message: `Task ${uuid} successfully completed. Dump file: ${fileTemplate}`
   })
-
-  definition = []
-}
-
-const createTableStatements = async () => {
-  const { connection } = await getConnection()
-
-  const results = await Promise.all(
-    definition.map(async ({ table }) => {
-      const [rows] = await connection.query<RowDataPacket[]>(
-        `SHOW CREATE TABLE ${table}`
-      )
-      return rows
-    })
-  )
-
-  return results.reduce(
-    (collected: string, next: RowDataPacket[]) =>
-      `${collected}${next[0]['Create Table']};`,
-    ''
-  )
-}
-
-const createDataStatements = async () => {
-  const { connection } = await getConnection()
-
-  let statements = ''
-  for (const config of definition) {
-    if (config.type === ExportTypes.STRUCTURE_ONLY) continue
-
-    const [rows] = await connection.query<RowDataPacket[]>(
-      `SELECT * FROM ${config.table}`
-    )
-
-    statements += createInsertStatements(config, rows)
-  }
-
-  return statements
-}
-
-const createInsertStatements = (
-  config: ConfigObject,
-  rows: RowDataPacket[]
-) => {
-  return `INSERT INTO ${config.table} VALUES${rows.reduce(valueReducer(config.columns), '').slice(0, -1)};\n\n`
-}
-
-const valueReducer =
-  (columnConfig: Record<string, CallableFunction> | undefined) =>
-  (values: string, next: Record<string, unknown>) => {
-    const row = Object.keys(next)
-      .reduce((collected: string, column: string) => {
-        return `${collected}${getValues(next[column], getFilterMethod(columnConfig, column))}, `
-      }, '')
-      .slice(0, -2)
-
-    return `${values} (${row}),`
-  }
-
-const getValues = (v: unknown, clb: CallableFunction) => {
-  const res = clb(v)
-
-  return typeof v === 'object' ? `"${res}"` : res
-}
-
-const getFilterMethod = (
-  columnConfig: Record<string, CallableFunction> | undefined,
-  column: string
-) => {
-  return (columnConfig ?? {})[column] ?? ((v: unknown) => v)
+  cleanUpTmpFiles()
 }
 
 const errorHandler = (uuid: string, { message }: Error) => {
