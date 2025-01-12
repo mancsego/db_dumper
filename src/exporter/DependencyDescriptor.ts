cbe76bc3562efd89dafaa148eddad9d4439bd40e
Istvan Abraham <mancsego@gmail.com>
2025-01-13 00:18:42 +0100

    Change to inner join to support where

--- src/exporter/DependencyDescriptor.ts
+++ src/exporter/DependencyDescriptor.ts
@@ -7,15 +7,16 @@ const saveHistory = (table: string, query: string): void => {
   history.insert({ table, query })
 }
 
-const getWherePartForDependency = (table: string, primaryColumn: string): string => {
+const getJoinForDependency = (agentTable: string, table: string, primaryColumn: string): string => {
   const entry = history.findOne({ table })
   if (!entry) return `"${primaryColumn}" = "${primaryColumn}"`
 
   const subSelect = entry.query
     .replace('SELECT *', `SELECT ${primaryColumn}`)
     .replace(';', '')
+  const subTable = `sub_${table}`
 
-  return `${primaryColumn} IN (${subSelect})`
+  return `INNER JOIN (${subSelect}) as ${subTable} ON ${subTable}.${primaryColumn} = ${agentTable}.${primaryColumn}`
 }
 
-export { saveHistory, getWherePartForDependency }
+export { saveHistory, getJoinForDependency }
--- src/exporter/StatementService.ts
+++ src/exporter/StatementService.ts
@@ -1,12 +1,15 @@
 import { RowDataPacket } from 'mysql2'
 import {
-  ImportDefinition,
+  ConfigObject,
   ExportTypes,
-  ConfigObject
+  ImportDefinition
 } from '#src/exporter/config/types'
 import { getConnection } from '#src/exporter/db'
 import { dumpStream } from '#src/exporter/FileService'
-import { getWherePartForDependency, saveHistory } from '#src/exporter/DependencyDescriptor'
+import {
+  getJoinForDependency,
+  saveHistory
+} from '#src/exporter/DependencyDescriptor'
 
 const stringify = ['object', 'string']
 const shouldStringify = (x: unknown) =>
@@ -34,9 +37,9 @@ const createDataStatements = async (definition: ImportDefinition) => {
   for (const config of definition) {
     if (config.type === ExportTypes.STRUCTURE_ONLY) continue
 
+    const limit = config.limit ? `LIMIT ${config.limit}` : ''
     const query = `SELECT *
-                   FROM ${config.table}
-                   WHERE ${await _createWhere(config)}`
+                   FROM ${config.table} ${await _createJoin(config)} ${limit};`
 
     saveHistory(config.table, query)
     const [rows] = await connection.query<RowDataPacket[]>(query)
@@ -51,28 +54,21 @@ const _createInsertStatements = (
   config: ConfigObject,
   rows: RowDataPacket[]
 ) => {
+  const values = rows.reduce(_valueReducer(config), '').slice(0, -1)
 
-  const values = rows
-    .reduce(_valueReducer(config), '')
-    .slice(0, -1)
-
-  const statement = `INSERT INTO ${config.table}
-                     VALUES ${values};  `
-
-  return statement
+  return `INSERT INTO ${config.table} VALUES ${values};  `
 }
 
 const _valueReducer =
-  (config: ConfigObject) =>
-    (values: string, next: Record<string, unknown>) => {
-      const row = Object.keys(next)
-        .reduce((collected: string, column: string) => {
-          return `${collected}${_getValue(next[column], _getFilterMethod(config.columns, column))}, `
-        }, '')
-        .slice(0, -2)
+  (config: ConfigObject) => (values: string, next: Record<string, unknown>) => {
+    const row = Object.keys(next)
+      .reduce((collected: string, column: string) => {
+        return `${collected}${_getValue(next[column], _getFilterMethod(config.columns, column))}, `
+      }, '')
+      .slice(0, -2)
 
-      return `${values} (${row}),`
-    }
+    return `${values} (${row}),`
+  }
 
 const _getValue = (v: unknown, clb: CallableFunction) => {
   const res = clb(v)
@@ -85,17 +81,14 @@ const _getFilterMethod = (
   column: string
 ) => (columnConfig ?? {})[column] ?? ((v: unknown) => v)
 
-const _createWhere = async (
-  config: ConfigObject
-) => {
-  if (!config.dependencies) return '1 = 1;'
+const _createJoin = async (config: ConfigObject) => {
+  if (!config.dependencies) return ''
 
-  const where = config.dependencies.reduce((wherePart, { table, column }) =>
-      `${wherePart} ${getWherePartForDependency(table, column)} AND`,
+  return config.dependencies.reduce(
+    (wherePart, { table, column }) =>
+      `${wherePart} ${getJoinForDependency(config.table, table, column)}`,
     ''
   )
-
-  return where.slice(0, -4)
 }
 
 export { createTableStatements, createDataStatements }
