b83a13209442e3e1a59b2d144f36e225ed043944
Istvan Abraham <mancsego@gmail.com>
2025-01-12 17:12:43 +0100

    Remove old dependency filter mechanism

--- src/exporter/FileService.ts
+++ src/exporter/FileService.ts
@@ -1,50 +1,9 @@
 import fs, { WriteStream } from 'fs'
 import path from 'path'
-import { ConfigObject } from './config/types'
-
-type PrimaryStream = {
-  save: CallableFunction
-  close: CallableFunction
-}
 
 const TMP_FOLDER = path.resolve('tmp')
 const DUMP_FILE = path.resolve('dumps/dump.sql')
 
-const openPrimaryStream = (table: string): PrimaryStream => {
-  const fileName = _createTmpFileName(table)
-  fs.unlink(fileName, () => {})
-  const stream = fs.createWriteStream(fileName)
-
-  return {
-    save: (primary: string | undefined) => {
-      if (!primary) return
-
-      stream.write(`${primary},`)
-    },
-    close: () => {
-      stream.close()
-    }
-  }
-}
-
-const readPrimaries = (() => {
-  let cache: Record<string, string[]> = {}
-
-  return ({ dependencies }: ConfigObject): Record<string, string[]> => {
-    dependencies?.forEach((table) => {
-      if (cache[table]) return
-
-      const primaries = fs
-        .readFileSync(_createTmpFileName(table), { encoding: 'utf8' })
-        .split(',')
-        .filter(Boolean)
-
-      cache = { ...cache, [table]: primaries }
-    })
-
-    return cache
-  }
-})()
 
 const cleanUpTmpFiles = (): void => {
   fs.readdir(TMP_FOLDER, (_, tmpFiles) => {
@@ -75,7 +34,4 @@ const dumpStream = (() => {
   }
 })()
 
-const _createTmpFileName = (table: string): string =>
-  `${TMP_FOLDER}/${table}_primaries.tmp`
-
-export { dumpStream, openPrimaryStream, readPrimaries, cleanUpTmpFiles }
+export { dumpStream, cleanUpTmpFiles }
--- src/exporter/StatementService.ts
+++ src/exporter/StatementService.ts
@@ -5,11 +5,7 @@ import {
   ConfigObject
 } from '#src/exporter/config/types'
 import { getConnection } from '#src/exporter/db'
-import {
-  dumpStream,
-  openPrimaryStream,
-  readPrimaries
-} from '#src/exporter/FileService'
+import { dumpStream } from '#src/exporter/FileService'
 
 const stringify = ['object', 'string']
 const shouldStringify = (x: unknown) =>
@@ -38,7 +34,9 @@ const createDataStatements = async (definition: ImportDefinition) => {
     if (config.type === ExportTypes.STRUCTURE_ONLY) continue
 
     const [rows] = await connection.query<RowDataPacket[]>(
-      `SELECT * FROM ${config.table} ${await _createWhere(definition, config)}`
+      `SELECT *
+       FROM ${config.table}
+       WHERE ${await _createWhere(config)}`
     )
 
     dumb.write(_createInsertStatements(config, rows))
@@ -51,26 +49,19 @@ const _createInsertStatements = (
   config: ConfigObject,
   rows: RowDataPacket[]
 ) => {
-  const stream = openPrimaryStream(config.table)
-  const savePrimary = (next: Record<string, unknown>) => {
-    stream.save(next[config.primary])
-  }
 
   const values = rows
-    .reduce(_valueReducer(config, savePrimary), '')
+    .reduce(_valueReducer(config), '')
     .slice(0, -1)
 
-  const statement = `INSERT INTO ${config.table} VALUES${values};\n`
-  stream.close()
+  const statement = `INSERT INTO ${config.table} VALUES ${values};  `
 
   return statement
 }
 
 const _valueReducer =
-  (config: ConfigObject, clb: CallableFunction) =>
+  (config: ConfigObject) =>
   (values: string, next: Record<string, unknown>) => {
-    clb(next)
-
     const row = Object.keys(next)
       .reduce((collected: string, column: string) => {
         return `${collected}${_getValue(next[column], _getFilterMethod(config.columns, column))}, `
@@ -92,24 +83,13 @@ const _getFilterMethod = (
 ) => (columnConfig ?? {})[column] ?? ((v: unknown) => v)
 
 const _createWhere = async (
-  definition: ImportDefinition,
   config: ConfigObject
 ) => {
-  if (!config.dependencies) return ''
+  if (!config.dependencies) return '1 = 1;'
 
-  const primaries = readPrimaries(config)
+  const where = config.dependencies.reduce((wherePart) => wherePart, '1 = 1;')
 
-  const where = config.dependencies.reduce((wherePart, dependency) => {
-    const keys = primaries[dependency]
-    if (!keys.length) return wherePart
-    const { primary } = definition.find(
-      ({ table }) => table === dependency
-    ) as ConfigObject
-
-    return `${wherePart} ${primary} IN (${keys}) AND`
-  }, 'WHERE')
-
-  return where.slice(0, -4)
+  return where
 }
 
 export { createTableStatements, createDataStatements }
